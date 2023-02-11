462dc5b28b42aa4e894be1a2e166a0b1d5eba8a6
Rajat9927 <Rajat9927kumar@gmail.com>
2023-02-11 20:55:37 +0530

    final

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
-fetch("https://dummyjson.com/products").then((response)=> response.json()).then((data)=>{
-  let primeData = data.products;
-  let divv = document.getElementById('divv'); // this stores the main div element where we gonaa display our cards
+fetch("https://dummyjson.com/products")
+.then((response)=> response.json())
+.then((data)=>{
+  let productData = data.products;
+  let add = document.getElementById('getdata');
 
-  // to loop over a primeData array of objects
-  for (let i = 0; i < primeData.length; i++) {
-    const element = primeData[i];
-    let categoryy = element.category;
-    let idd = element.id;
-    let brandd = element.brand;
-    let titlee = element.title;
-    let descriptionn = element.description;
-    let pricee = element.price;
-    let discountPercentagee = element.discountPercentage;
-    let stockk = element.stock;
-    let ratingg = element.rating;
+  // we use foor loop to access all product details.
+  for (let i = 0; i < productData.length; i++) {
+    const element = productData[i];
+    let prodCategory = element.category;
+    let idNumber = element.id;
+    let prodBrand = element.brand;
+    let prodTitle = element.title;
+    let prodDescription = element.description;
+    let prodPrice = element.price;
+    let dis = element.discountPercentage;
+    let prodStock = element.stock;
+    let prodRating = element.rating;
     let imageArr = element.images;
     let imagee = imageArr[0];
 
-    let spann = document.createElement("span");
+    let crt = document.createElement("span");
 
-    spann.innerHTML = 
+    //here wee are creating html tag with the help of java script, inside for loop. It will create html for all elements
+
+    crt.innerHTML = 
     `<div class="card border border-dark border-3 border-opacity-25" style="width: 18rem;">
     <img src="${imagee}" width="100%" height="180" class="card-img-top" alt="no image available">
   
     <div class="card-body">
-      <h3 class="card-title">${brandd}</h3>
-      <p class="card-text text-capitalize"> <b>Category:</b> ${categoryy}</p>
-      <p class="card-text text-capitalize"> <b>id:</b> ${idd}</p>
-      <p class="card-text text-capitalize"> <b>Model/Title:</b> ${titlee}</p>
-      <p class="card-text text-capitalize fst-italic"> <b>Description:</b> ${descriptionn}</p>
-      <p class="card-text text-capitalize"> <b>Price:</b> ${pricee}$</p>
-      <p class="card-text text-capitalize"> <b>Special Discount for you:</b> ${discountPercentagee}%</p>
-      <h5> <span class="badge text-bg-success"><b>Ratings:</b> ${ratingg}</span></h5>
-      <p class="card-text text-capitalize fw-semibold text-primary"><i>Only ${stockk} items remain in stock. Hurry up before stock gets exausted!!!<i></p>
-      <a class="btn btn-dark">Buy Now</a>
+      <h3 class="card-title">${prodBrand}</h3>
+      <p class="card-text text-capitalize"> <b>Product Category:</b> ${prodCategory}</p>
+      <p class="card-text text-capitalize"> <b>ID:</b> ${idNumber}</p>
+      <p class="card-text text-capitalize"> <b>Model:</b> ${prodTitle}</p>
+      <p class="card-text text-capitalize fst-italic"> <b>Product Description:</b> ${prodDescription}</p>
+      <p class="card-text text-capitalize"> <b>Price:</b> ${prodPrice}$</p>
+      <p class="card-text text-capitalize"> <b>Discouunt:</b> ${dis}%</p>
+      <h5> <span class="badge text-bg-success"><b>Ratings:</b> ${prodRating}</span></h5>
+      <p class="card-text text-capitalize fw-semibold text-primary"><i>Only ${prodStock} stock me hai<i></p>
+      <a class="btn btn-dark">Add to Cart</a>
     </div>
   </div>`
-    divv.appendChild(spann);
+  add.appendChild(crt);
   }
   
-  console.log(primeData);
+  console.log(productData);
 
 })
